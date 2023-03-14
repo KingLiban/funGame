d7e1c2b8cb7bbf4a388543073a8816b6b905b8ab
KingLiban <113272384+KingLiban@users.noreply.github.com>
2023-03-13 22:15:51 -0400

    Add files via upload

--- src/Inputs/KeyboardInputs.java
+++ src/Inputs/KeyboardInputs.java
@@ -1,9 +1,18 @@
 package Inputs;
 
+import main.Game;
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.security.Key;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+    public KeyboardInputs(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -14,16 +23,16 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()){
             case KeyEvent.VK_W:
-                System.out.println("W");
+                gamePanel.changeYDelta(-5);
                 break;
             case KeyEvent.VK_A:
-                System.out.println("A");
+                gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S");
+                gamePanel.changeYDelta(5);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D");
+                gamePanel.changeXDelta(5);
                 break;
         }
     }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
     package main;
     import Inputs.KeyboardInputs;
+    import Inputs.MouseInputs;
 
     import java.awt.Graphics;
     import java.awt.event.KeyEvent;
@@ -7,12 +8,36 @@
     import javax.swing.JPanel;
 
     public class GamePanel extends JPanel{
+        MouseInputs mouseInputs = new MouseInputs(this);
+        KeyboardInputs keyboardInputs = new KeyboardInputs(this);
+        private int xDelta = 100;
+        private int yDelta = 100;
         public GamePanel(){
-            addKeyListener(new KeyboardInputs());
+            setFocusable(true);
+            requestFocus(true);
+            addKeyListener(keyboardInputs);
+            addMouseListener(mouseInputs);
+            addMouseMotionListener(mouseInputs);
 
+        }
+
+        public void changeXDelta(int value) {
+            this.xDelta += value;
+            repaint();
+        }
+
+        public void changeYDelta(int value) {
+            this.yDelta += value;
+            repaint();
+        }
+
+        public void newRecPos(int x, int y){
+            this.xDelta = x;
+            this.yDelta = y;
+            repaint();
         }
         public void paintComponent(Graphics g){
             super.paintComponent(g);
-            g.fillArc(300,40,50,60,30, 40);
+            g.fillRect(xDelta, yDelta,50,60);
         }
     }
