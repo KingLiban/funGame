308227e4b1c8b460d70f5f922f576b9f709e6a0b
KingLiban <113272384+KingLiban@users.noreply.github.com>
2023-03-15 18:03:46 -0400

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,19 +2,27 @@
     import Inputs.KeyboardInputs;
     import Inputs.MouseInputs;
 
-    import java.awt.Graphics;
+    import java.awt.*;
     import java.awt.event.KeyEvent;
     import java.awt.event.KeyListener;
+    import java.util.Random;
     import javax.swing.JPanel;
 
     public class GamePanel extends JPanel{
         MouseInputs mouseInputs = new MouseInputs(this);
         KeyboardInputs keyboardInputs = new KeyboardInputs(this);
-        private int xDelta = 100;
-        private int yDelta = 100;
+        private float xDelta = 100;
+        private float yDelta = 100;
+        private float xDir = 0.03f;
+        private float yDir = 0.03f;
+        private int frames = 0;
+        private long lastCheck = 0;
+        private Color color = new Color(100, 25, 40);
+        private Random random;
         public GamePanel(){
             setFocusable(true);
             requestFocus(true);
+            random = new Random();
             addKeyListener(keyboardInputs);
             addMouseListener(mouseInputs);
             addMouseMotionListener(mouseInputs);
@@ -23,21 +31,48 @@
 
         public void changeXDelta(int value) {
             this.xDelta += value;
-            repaint();
         }
 
         public void changeYDelta(int value) {
             this.yDelta += value;
-            repaint();
         }
 
         public void newRecPos(int x, int y){
             this.xDelta = x;
             this.yDelta = y;
-            repaint();
         }
         public void paintComponent(Graphics g){
             super.paintComponent(g);
-            g.fillRect(xDelta, yDelta,50,60);
+            updateRectangle();
+            g.setColor(color);
+            g.fillRect((int) xDelta, (int) yDelta,50,60);
+            repaint();
+            frames++;
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+
+        private void updateRectangle() {
+            xDelta+=xDir;
+            if(xDelta > 400 || xDelta < 0){
+                xDir*=-1;
+                color = getRndColor();
+            }
+            yDelta+=yDir;
+            if(yDelta > 400 || yDelta < 0){
+                yDir*=-1;
+                color = getRndColor();
+            }
+
+        }
+
+        private Color getRndColor() {
+            int r = random.nextInt(255);
+            int g = random.nextInt(255);;
+            int b = random.nextInt(255);;
+            return new Color(r, g, b);
         }
     }
