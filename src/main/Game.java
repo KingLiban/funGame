f7d995c2d3deaf61a18d856508175f7c3fa9197a
KingLiban <113272384+KingLiban@users.noreply.github.com>
2023-03-15 22:36:11 -0400

    Update Game.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -11,10 +11,32 @@ public class Game implements Runnable{
         gamePanel = new GamePanel();
         GameWindow gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
     }
 
+    private void startGameLoop(){
+        thread = new Thread(this);
+        thread.start();
+    }
     @Override
     public void run() {
-
+        double timePerFrame =  1.0 / 120.0 * 1000000000.0;;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+        while (true){
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame){
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
     }
 }
