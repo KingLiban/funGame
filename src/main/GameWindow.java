495864b1e63f79262b25a5167dec2e0a7e06a48a
KingLiban <113272384+KingLiban@users.noreply.github.com>
2023-03-15 22:37:17 -0400

    Update GameWindow.java

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -6,7 +6,6 @@ public class GameWindow {
     private JFrame jframe;
     public GameWindow(GamePanel gamePanel){
         JFrame jframe = new JFrame();
-        jframe.setSize(400,400);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
         jframe.setLocationRelativeTo(null);
